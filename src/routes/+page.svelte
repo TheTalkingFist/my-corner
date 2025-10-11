7ff0995efea9ada84317fe543f201f08bf6577b4
TheTalkingFist <mikhail19871983@gmail.com>
2025-10-11 14:28:04 +0800

    Title change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+
+</script>
+
+<h1>The Talking Fist's Corner of the Internet</h1>
+
+<style>
+    h1 {
+        text-align: center;
+        color: white;
+        -webkit-text-stroke: 0.5px black;
+        font-size: 3em;
+    }
+</style>
